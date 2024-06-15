527f28eba2f4615894f2c6d60eff649973c15810
Bishwajit <bishwajit0020@gmail.com>
2024-06-15 17:02:40 +0600

    (nvim) Add neovim with lazy conf

---
+++ dot_config/nvim/lua/plugins/gruvbox.lua
@@ -0,0 +1,17 @@
+return {
+  -- add gruvbox
+  -- { "ellisonleao/gruvbox.nvim" },
+  --
+  -- -- Configure LazyVim to load gruvbox
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "gruvbox",
+  --   },
+  -- },
+  {
+    -- This plugin adds the ability to navigate between vim and tmux splits using the same keybindings
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+}
